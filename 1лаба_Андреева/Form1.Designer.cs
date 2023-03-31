94640c7d555b80d1ab1e11251066d1f6d66a4a20
OxiD1Zed <skabo1000@mail.ru>
2023-03-31 23:00:32 +0300

    Начальный код

---
+++ 1лаба_Андреева/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace _1лаба_Андреева
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonFile = new System.Windows.Forms.Button();
+            this.buttonRandom = new System.Windows.Forms.Button();
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonFile
+            // 
+            this.buttonFile.Location = new System.Drawing.Point(23, 12);
+            this.buttonFile.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonFile.Name = "buttonFile";
+            this.buttonFile.Size = new System.Drawing.Size(165, 42);
+            this.buttonFile.TabIndex = 0;
+            this.buttonFile.Text = "Загрузить массив из файла";
+            this.buttonFile.UseVisualStyleBackColor = true;
+            this.buttonFile.Click += new System.EventHandler(this.buttonFile_Click);
+            // 
+            // buttonRandom
+            // 
+            this.buttonRandom.Location = new System.Drawing.Point(23, 59);
+            this.buttonRandom.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonRandom.Name = "buttonRandom";
+            this.buttonRandom.Size = new System.Drawing.Size(165, 37);
+            this.buttonRandom.TabIndex = 1;
+            this.buttonRandom.Text = "Сгененировать";
+            this.buttonRandom.UseVisualStyleBackColor = true;
+            this.buttonRandom.Click += new System.EventHandler(this.buttonRandom_Click);
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.Location = new System.Drawing.Point(23, 126);
+            this.buttonSort.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(165, 39);
+            this.buttonSort.TabIndex = 2;
+            this.buttonSort.Text = "Сортировка слиянием";
+            this.buttonSort.UseVisualStyleBackColor = true;
+            this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "________________________";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(467, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Первоначальный массив";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(379, 59);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(353, 106);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(467, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(199, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Отсортированный массив";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(379, 217);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(353, 106);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(375, 366);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Время выполнения";
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.Location = new System.Drawing.Point(555, 366);
+            this.timerLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(0, 16);
+            this.timerLabel.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(299, 22);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(33, 22);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(344, 22);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(33, 22);
+            this.textBox4.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(332, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(11, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.timerLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonSort);
+            this.Controls.Add(this.buttonRandom);
+            this.Controls.Add(this.buttonFile);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Сортировка";
+            this.TransparencyKey = System.Drawing.Color.Chartreuse;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonFile;
+        private System.Windows.Forms.Button buttonRandom;
+        private System.Windows.Forms.Button buttonSort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ 1лаба_Андреева/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1лаба_Андреева
+{
+    public partial class Form1 : Form
+    {  
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BackColor = Color.AntiqueWhite;
+        }
+
+        private void buttonFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = @"C:\Users\";
+            openFileDialog.Filter = "|*.txt";
+            try
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    FileInfo file = new FileInfo(openFileDialog.FileName);
+                    using (StreamReader sr = new StreamReader(file.OpenRead()))
+                    {
+                        textBox1.Text = sr.ReadToEnd();
+                    }
+                }
+                else
+                {
+                    throw new FormatException("Файл не выбран");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonRandom_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Random r = new Random();
+                string array = "";
+                int arraySize = r.Next(textBox3.Text != "" ? int.Parse(textBox3.Text.Trim()) : 2,
+                                       textBox4.Text != "" ? int.Parse(textBox4.Text.Trim()) : 1000);
+                for (int i = 0; i < arraySize; i++)
+                {
+                    array += r.Next(10000) + " ";
+                }
+                textBox1.Text = array;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            string[] _arr = textBox1.Text.Trim(' ').Split(' ', ',', '.');
+            int[] arr = new int[_arr.Length];
+            try
+            {
+                
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = int.Parse(_arr[i]);
+                }
+                arr = mergeSort(arr);
+                textBox2.Text = String.Join(" ", arr);
+                timerLabel.Text = (sw.ElapsedMilliseconds / 1000d) + " сек.";
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sw.Stop();
+            }
+            
+
+            int[] mergeSort(int[] array)
+            {
+                int[] left;
+                int[] right;
+                int[] result = new int[array.Length];
+
+                if (array.Length <= 1)
+                    return array;
+    
+                int midPoint = array.Length / 2;
+                
+                left = new int[midPoint];
+
+                if (array.Length % 2 == 0)
+                    right = new int[midPoint];
+                
+                else
+                    right = new int[midPoint + 1];
+                
+                for (int i = 0; i < midPoint; i++)
+                    left[i] = array[i];
+                 
+                int x = 0;
+                
+            for (int i = midPoint; i < array.Length; i++)
+                {
+                    right[x] = array[i];
+                    x++;
+                }
+                
+                left = mergeSort(left);
+                
+                right = mergeSort(right);
+                //Merge our two sorted arrays
+                result = merge(left, right);
+                return result;
+            }
+
+            //This method will be responsible for combining our two sorted arrays into one giant array
+            int[] merge(int[] left, int[] right)
+            {
+                int resultLength = right.Length + left.Length;
+                int[] result = new int[resultLength];
+                //
+                int indexLeft = 0, indexRight = 0, indexResult = 0;
+                //while either array still has an element
+                while (indexLeft < left.Length || indexRight < right.Length)
+                {
+                    //if both arrays have elements  
+                    if (indexLeft < left.Length && indexRight < right.Length)
+                    {
+                        //If item on left array is less than item on right array, add that item to the result array 
+                        if (left[indexLeft] <= right[indexRight])
+                        {
+                            result[indexResult] = left[indexLeft];
+                            indexLeft++;
+                            indexResult++;
+                        }
+                        // else the item in the right array wll be added to the results array
+                        else
+                        {
+                            result[indexResult] = right[indexRight];
+                            indexRight++;
+                            indexResult++;
+                        }
+                    }
+                    //if only the left array still has elements, add all its items to the results array
+                    else if (indexLeft < left.Length)
+                    {
+                        result[indexResult] = left[indexLeft];
+                        indexLeft++;
+                        indexResult++;
+                    }
+                    //if only the right array still has elements, add all its items to the results array
+                    else if (indexRight < right.Length)
+                    {
+                        result[indexResult] = right[indexRight];
+                        indexRight++;
+                        indexResult++;
+                    }
+                }
+                return result;
+            }
+        }
+    }
+}
